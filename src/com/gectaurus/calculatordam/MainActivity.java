e6cba29413b9da71f2c4537733d22a040080a5df
albaoe <gectorus@gmail.com>
2014-09-24 10:38:15 +0200

    Moved the buttons to the onCreate (fixed crash)
    Make the 0 add 0s.

--- src/com/gectaurus/calculatordam/MainActivity.java
+++ src/com/gectaurus/calculatordam/MainActivity.java
@@ -12,38 +12,40 @@ import android.widget.EditText;
 
 public class MainActivity extends Activity {
 	
-	EditText toptext = (EditText) findViewById(R.id.editText1);
-	EditText bottomtext = (EditText) findViewById(R.id.EditText01);
-
-	Button button0 = (Button) findViewById(R.id.Button02);
-	Button button1 = (Button) findViewById(R.id.button11);
-	Button button2 = (Button) findViewById(R.id.button10);
-	Button button3 = (Button) findViewById(R.id.button9);
-	Button button4 = (Button) findViewById(R.id.button7);
-	Button button5 = (Button) findViewById(R.id.button6);
-	Button button6 = (Button) findViewById(R.id.button5);
-	Button button7 = (Button) findViewById(R.id.button1);
-	Button button8 = (Button) findViewById(R.id.button2);
-	Button button9 = (Button) findViewById(R.id.button3);
-	Button buttondot = (Button) findViewById(R.id.Button01);
-	Button buttondel = (Button) findViewById(R.id.Button03);
-	Button buttonadd = (Button) findViewById(R.id.Button04);
-	Button buttonsubtract = (Button) findViewById(R.id.button12);
-	Button buttonmultiply = (Button) findViewById(R.id.button8);
-	Button buttondivide = (Button) findViewById(R.id.button4);
-	Button buttonequal = (Button) findViewById(R.id.button13);	
+		
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        final EditText toptext = (EditText) findViewById(R.id.editText1);
+    	final EditText bottomtext = (EditText) findViewById(R.id.EditText01);
+
+    	final Button button0 = (Button) findViewById(R.id.Button02);
+    	final Button button1 = (Button) findViewById(R.id.button11);
+    	final Button button2 = (Button) findViewById(R.id.button10);
+    	final Button button3 = (Button) findViewById(R.id.button9);
+    	final Button button4 = (Button) findViewById(R.id.button7);
+    	final Button button5 = (Button) findViewById(R.id.button6);
+    	final Button button6 = (Button) findViewById(R.id.button5);
+    	final Button button7 = (Button) findViewById(R.id.button1);
+    	final Button button8 = (Button) findViewById(R.id.button2);
+    	final Button button9 = (Button) findViewById(R.id.button3);
+    	final Button buttondot = (Button) findViewById(R.id.Button01);
+    	final Button buttondel = (Button) findViewById(R.id.Button03);
+    	final Button buttonadd = (Button) findViewById(R.id.Button04);
+    	final Button buttonsubtract = (Button) findViewById(R.id.button12);
+    	final Button buttonmultiply = (Button) findViewById(R.id.button8);
+    	final Button buttondivide = (Button) findViewById(R.id.button4);
+    	final Button buttonequal = (Button) findViewById(R.id.button13);
+        
         button0.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				bottomtext.setText("0");	
+				bottomtext.setText(bottomtext.getText() + "0");	
 			}
 		});
     }
