bfc2b324cc1e079b743f91b69eda59a0e41e7321
albaoe <gectorus@gmail.com>
2014-09-24 02:00:11 +0200

    Lot of things.
    Added buttons, text fields, their @strings and created in the activity
    but the app crashes.

--- src/com/gectaurus/calculatordam/MainActivity.java
+++ src/com/gectaurus/calculatordam/MainActivity.java
@@ -4,14 +4,48 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
+	
+	EditText toptext = (EditText) findViewById(R.id.editText1);
+	EditText bottomtext = (EditText) findViewById(R.id.EditText01);
 
+	Button button0 = (Button) findViewById(R.id.Button02);
+	Button button1 = (Button) findViewById(R.id.button11);
+	Button button2 = (Button) findViewById(R.id.button10);
+	Button button3 = (Button) findViewById(R.id.button9);
+	Button button4 = (Button) findViewById(R.id.button7);
+	Button button5 = (Button) findViewById(R.id.button6);
+	Button button6 = (Button) findViewById(R.id.button5);
+	Button button7 = (Button) findViewById(R.id.button1);
+	Button button8 = (Button) findViewById(R.id.button2);
+	Button button9 = (Button) findViewById(R.id.button3);
+	Button buttondot = (Button) findViewById(R.id.Button01);
+	Button buttondel = (Button) findViewById(R.id.Button03);
+	Button buttonadd = (Button) findViewById(R.id.Button04);
+	Button buttonsubtract = (Button) findViewById(R.id.button12);
+	Button buttonmultiply = (Button) findViewById(R.id.button8);
+	Button buttondivide = (Button) findViewById(R.id.button4);
+	Button buttonequal = (Button) findViewById(R.id.button13);	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        button0.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				bottomtext.setText("0");	
+			}
+		});
     }
 
 
